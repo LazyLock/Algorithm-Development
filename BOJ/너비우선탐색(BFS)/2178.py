0dbe64239aee710aa8c3197cc5e362d5a3fcc25d
LAPTOP-75VSOBJ5\a0105 <a01058121097@gmail.com>
2021-09-03 14:29:16 +0900

    2178 [BFS 알고리즘]
    deque 모듈을 이용한 구현. dx, dy 설정하는 것 뿐만아니라 전체적인 과정이 굉장히 알고리즘스럽다.

---
+++ BOJ/너비우선탐색(BFS)/2178.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+tree = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+queue = deque()
+queue.append([0,0])
+
+while queue:
+    x, y = queue.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+
+        if tree[nx][ny] == 0:
+            continue
+
+        if tree[nx][ny] == 1:
+            queue.append([nx, ny])
+            tree[nx][ny] = tree[x][y] + 1
+
+print(tree[n - 1][m - 1])

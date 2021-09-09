8b077c05beeca62e2e05c9f01fa550d06828dd2a
LAPTOP-75VSOBJ5\a0105 <a01058121097@gmail.com>
2021-09-09 16:53:56 +0900

    7576 [BFS 알고리즘]
    배열 두개를 이용하여 갱신하는 풀이. 시간이 조금 더 효율적이다.

--- BOJ/너비우선탐색(BFS)/7576.py
+++ BOJ/너비우선탐색(BFS)/7576.py
@@ -1,29 +1,39 @@
-from collections import deque
 import sys
 
 m, n = map(int, sys.stdin.readline().split())
 
-tree = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-queue = deque()
-day = -2
+tree = []
+queue = []
+next_queue = []
+day = 0
+is_full = False
 
-for i in range(m):
-    for j in range(n):
-        if tree[j][i] == 1:
-            queue.append([j, i])
+for i in range(n):
+    temp = list(map(int, sys.stdin.readline().split()))
+    for j, k in enumerate(temp):
+        if k == 1:
+            queue.append([i, j])
+    tree.append(temp)
+
+if len(queue) == m * n:
+    is_full = True
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 while queue:
-    y, x = queue.popleft()
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and tree[ny][nx] == 0:
-            queue.append([ny, nx])
-            tree[ny][nx] = tree[y][x] + 1
+    for y, x in queue:
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and tree[ny][nx] == 0:
+                next_queue.append([ny, nx])
+                tree[ny][nx] = 1
+
+    queue = next_queue
+    next_queue = []
+    day += 1
 
 flag = False
 
@@ -32,13 +42,12 @@ for i in tree:
         if j == 0:
             flag = True
             break
-        day = max(day, j)
     if flag:
         print(-1)
         break
 
 if not flag:
-    if day == -1:
+    if is_full:
         print(0)
     else:
         print(day - 1)
